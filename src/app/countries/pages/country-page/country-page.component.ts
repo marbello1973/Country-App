7603c49873fa39c911b95082a7c1f36276156f98
marbello1973 <david.marbello@outlook.com>
2023-08-20 14:07:22 -0500

    commit

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class CountryPageComponent {
-
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private countriesService: CountriesService
+  ) {}
+  ngOnInit(): void {
+    this.ActivatedRoute.params.subscribe(({ id }) => {
+      this.countriesService
+        .searchCountryByAlphaCode(id)
+        .subscribe((country) => {
+          console.log(country);
+        });
+    });
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,16 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
+  private regionUrl: string = 'https://restcountries.com/v3.1/region/';
 
   constructor(private http: HttpClient) {}
 
+  searchCountryByAlphaCode(code: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => {
+        window.alert('Capital no encontrada');
+        return of([]);
+      })
+    );
+  }
+
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => {
+        window.alert('Capital no encontrada');
+        return of([]);
+      })
+    );
+  }
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => {
+        window.alert('Nombre no encontrada');
+        return of([]);
+      })
+    );
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => {
+        window.alert('Region no encontrada');
+        return of([]);
+      })
+    );
   }
 }
