e06503ead2b644d124b63278a56a5b4a138d1f40
marbello1973 <david.marbello@outlook.com>
2023-08-20 22:41:20 -0500

    adicionar componente country-page.componente.html y ts

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -8,17 +10,24 @@ import { CountriesService } from '../../services/countries.service';
   styles: [],
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private ActivatedRoute: ActivatedRoute,
-    private countriesService: CountriesService
+    private countriesService: CountriesService,
+    private router: Router
   ) {}
+
   ngOnInit(): void {
-    this.ActivatedRoute.params.subscribe(({ id }) => {
-      this.countriesService
-        .searchCountryByAlphaCode(id)
-        .subscribe((country) => {
-          console.log(country);
-        });
-    });
+    this.ActivatedRoute.params
+      .pipe(
+        switchMap(({ id }) =>
+          this.countriesService.searchCountryByAlphaCode(id)
+        )
+      )
+      .subscribe((country) => {
+        if (!country) return this.router.navigateByUrl('');
+        return (this.country = country);
+      });
   }
 }
